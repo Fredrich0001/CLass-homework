2772a5345d325842b463b815075dd357abe68510
Fredrich0001 <fredrichmatiku@gmail.com>
2023-02-13 15:44:53 +0300

    first commit

---
+++ form.js
@@ -0,0 +1,22 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+if ( email == "fredrichmatiku@gmail.com" && password == "fredrich"){
+alert ("Login successfully");
+window.location = "index2.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("email").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
